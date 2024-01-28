1a714b7846006831016614234b1cddd2fdd44448
Educls <eduardo.leao.pb@compasso.com.br>
2024-01-28 19:08:50 -0300

    update

--- src/services/login_service.js
+++ src/services/login_service.js
@@ -9,8 +9,10 @@ exports.verificaSeEmailUsuarioExistente = async (req) => {
     await db.connect();
 
     const {email, password} = req.body;
+    console.log(email + password)
 
     const rows = await db.query(constants.SQL_SELECT_LOGIN, [email, password, 'ativo'])
+    console.log(rows)
     await db.close();
 
     return rows
